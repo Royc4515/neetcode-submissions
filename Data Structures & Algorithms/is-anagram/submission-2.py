8822ba18bf604f030d9cec0c7063fc67216aad20
Roy Carmelli <Roy.y.carmelli@gmail.com>
2026-05-20 00:38:01 +0300

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False 
+        #idea: indexing all chars into a list, run on one list will
+        my_list = [0] * 26
+        #for every char occurence in s add 1 to the char index, for every occurence in t decrement 1 from the char index
+        for i, j in zip(s, t):
+            my_list[ord(i)-ord('a')] = my_list[ord(i)-ord('a')] + 1
+            my_list[ord(j)-ord('a')] = my_list[ord(j)-ord('a')] -1
+
+        return all(x == 0 for x in my_list) #if both has same chars exactly the list should be zeroed
